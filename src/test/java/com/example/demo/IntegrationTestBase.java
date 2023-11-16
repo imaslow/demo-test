1f296cf706813870dab3f3bc522a17c79c12ddd3
imaslow <ilya.maslov.93@list.ru>
2023-11-16 06:51:03 +0300

    add integration container

--- src/test/java/com/example/demo/IntegrationTestBase.java
+++ src/test/java/com/example/demo/IntegrationTestBase.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import com.example.demo.initializers.PostgresSqlContainer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
+@TestPropertySource("/demo-test.yml")
 @SpringBootTest
-class IntegrationTestBase {
+@ContextConfiguration(initializers = {
+		PostgresSqlContainer.Initializer.class
+})
+@WithMockUser
+@AutoConfigureMockMvc
+public abstract class IntegrationTestBase {
 
-	@Test
-	void contextLoads() {
+	@BeforeAll
+	static void init() {
+		PostgresSqlContainer.container.start();
 	}
 
+	@Autowired
+	protected MockMvc mockMvc;
+
+	@Autowired
+	protected ObjectMapper objectMapper;
 }
